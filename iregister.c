b1b0899453f947d8abc6d76e341877a51d327f60
Erik Wärmegård <erikwarmegard@outlook.com>
2020-09-08 11:31:07 +0200

    Corrected code structure, added functions defined in h-file

--- iregister.c
+++ iregister.c
@@ -7,21 +7,34 @@
 #include <stdio.h>
 #include "iregister.h"
 
-void resetBit(int i, iRegister *r) {
-  r->content &= ~(1 << i);
-}
-
 
 void resetAll(iRegister *r){
- if(r==Null) return;
- //fetch the register r into a variable
- //AND OperATION: with 0 in all 32 bit => to clear them
- //insert the value in r 
- 
+	if (r == Null) return;
+	//fetch the register r into a variable
+	//AND OperATION: with 0 in all 32 bit => to clear them
+	//insert the value in r 
+
 }
 
 
 void setBit(int i, iRegister *r){
- if(r==Null) return;
- r->content |= ~(1 << i);	
+	if (r == Null) return;
+	r->content |= ~(1 << i);	
+}
+
+
+void setAll(iRegister *r) {
+	if (r == Null) return;
+	r->content |= ~(content << 32); //Shift all bits 32 steps, makes them 0. Then invert for all 1's
+}
+
+
+int getBit(int i, iRegister r*) {
+	if (i > 31 || i < 0) return;
+	if (r == Null) return;
+	// TODO: Return statement here 
+}
+
+void resetBit(int i, iRegister *r) {
+	r->content &= ~(1 << i);
 }
