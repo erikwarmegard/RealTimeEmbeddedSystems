5ed5742ecf9c784fdd78d3e5c1a72939d4d8257b
Johan <johan.bergius96@gmail.com>
2020-09-23 21:11:18 +0200

    Ladda_ner detta och kör make erik!

---
+++ lab2_part23/lab1.c
@@ -0,0 +1,53 @@
+/*
+    Part of the Real-Time Embedded Systems course at Halmstad University
+    Copyright (c) 2017, Sebastian Kunze <sebastian.kunze@hh.se>
+    All rights reserved.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <rpi3.h>
+#include "exponential.h"
+
+void piface_puts(char c[]);
+void piface_putc(char c);
+void piface_clear();
+
+
+void delay(long unsigned int delayTime) {
+    long unsigned int i = 0;
+
+    while(i <= delayTime){
+        i++;
+    }
+    return;
+}
+
+
+int main()
+{
+
+    piface_clear();
+    while (1)
+    {
+
+      for(int i=1; i<20; i++){
+          char * c = malloc(sizeof(char)*32);
+          sprintf(c,"%d: ",i);
+          piface_puts(c);
+          ExpStruct * e1 = iexp((int)i);
+          char * f1 = expo2string(e1);
+          //sprintf(temp, "%d.%02d\n",exp->expInt,exp->expFraction);
+          piface_puts(f1);
+          //double realEXP =exp(i);
+          //printf("%d: %d.%d, \t real: %f \n",i,e1->expInt,e1->expFraction, realEXP);
+          free(f1);
+          free(e1);
+          free(c);
+          //dealy()
+          delay(2100000);
+      }
+
+    }
+
+	return 0;
+}
