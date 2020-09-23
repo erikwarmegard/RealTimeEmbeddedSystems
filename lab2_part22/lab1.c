10dbbf57257386cc87e4e7cadd815ed1454009bb
Johan <johan.bergius96@gmail.com>
2020-09-23 18:46:57 +0200

    jsut updated the main for task22

--- lab2_part22/lab1.c
+++ lab2_part22/lab1.c
@@ -6,25 +6,46 @@
 
 #include <stdio.h>
 #include <rpi3.h>
+#include "exponential.h"
 
 void piface_puts(char c[]);
 void piface_putc(char c);
 void piface_clear();
 
+void delay(long unsigned int delayTime) {
+    long unsigned int i = 0;
+
+    while(i <= delayTime){
+        i++;
+    }
+    return;
+}
+
+
 int main()
 {
-    /* Enable GPIO16 as an output */
-    GPIO->GPFSEL1 |= (1 << 18);
-    
-    /* Turn LED on */
-    GPIO->GPSET0 |= (1 << 16);
-    
-    /* Write to PiFace's LCD screen */    
-    piface_puts("Hello World!\n");
 
+    piface_clear();
     while (1)
     {
-      
+
+      for(int i=1; i<20; i++){
+          char * c = malloc(sizeof(char)*32);
+          sprintf(c,"%d: ",i);
+          piface_puts(c);
+          ExpStruct *e1 = (ExpStruct *) malloc(sizeof(ExpStruct));
+          e1 = iexp(i);
+          char * c1 = exponent2str(e1);
+          piface_puts(c1);
+          //double realEXP =exp(i);
+          //printf("%d: %d.%d, \t real: %f \n",i,e1->expInt,e1->expFraction, realEXP);
+          free(c1);
+          free(e1);
+          free(c);
+          //dealy()
+          delay(1400000);
+      }
+
     }
 
 	return 0;
