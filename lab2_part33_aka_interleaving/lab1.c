0223d2dd045430eb7612798132abdd06a54475eb
Johan <johan.bergius96@gmail.com>
2020-09-24 13:36:33 +0200

    changed the name of maps and added the correct Manual interleaving

---
+++ lab2_part33_aka_interleaving/lab1.c
@@ -0,0 +1,52 @@
+/*
+    Part of the Real-Time Embedded Systems course at Halmstad University
+    Copyright (c) 2017, Sebastian Kunze <sebastian.kunze@hh.se>
+    All rights reserved.
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <rpi3.h>
+#include "exponential.h"
+
+void piface_puts(char c[]);
+void piface_putc(char c);
+void piface_clear();
+
+
+
+
+void showExponentOnScreen(int i){
+  ExpStruct * e1 = iexp((int)i);  
+  piface_clear(); //clear screen
+  char * c = malloc(sizeof(char)*32);
+  sprintf(c,"%d: ",i);
+  piface_puts(c);
+  char * f1 = expo2string(e1);
+  piface_puts(f1);
+  free(f1);
+  free(e1);
+  free(c);
+
+
+}
+
+
+
+int main()
+{
+  /* Enable GPIO16 as an output */
+  GPIO->GPFSEL1 |= (1 << 18);
+  GPIO->GPCLR0 = (1 << 16);
+  piface_clear();
+  while (1){
+
+    for(int i=1; i<20; i++){
+        showExponentOnScreen(i);
+	
+  	
+    }
+
+  }
+
+	return 0;
+}
